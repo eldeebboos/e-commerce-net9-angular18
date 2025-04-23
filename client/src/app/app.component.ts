d1d89c169e3df1b3e47ca47ab80fba165933e10e
eldeebboos <eldeeb_boos@yahoo.com>
2025-04-23 23:52:04 +0200

    add angular client project with angular material and tailwind css, also test the connection to api

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, Inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
+import { environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  url = environment.apiUrl + 'products';
+  http = inject(HttpClient);
+  products: Product[] = [];
   title = 'client';
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>(this.url).subscribe((response) => {
+      this.products = response.data;
+      console.log(response);
+    });
+  }
 }
---
+++ client/src/app/layout/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { MatBadge } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatIcon, MatBadge, MatButtonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {}
