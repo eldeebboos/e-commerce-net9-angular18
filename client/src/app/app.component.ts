b49bad8521f9ef5a174385114ba9c5f585c70378
eldeebboos <eldeeb_boos@yahoo.com>
2025-05-10 22:45:23 +0300

    UI with angular, adding shop service, shop page with filter/sort/pagination/search, using tailwind and angular material

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,27 +1,14 @@
-import { Component, inject, Inject, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 import { HeaderComponent } from './layout/header/header.component';
-import { environment } from '../environments/environment';
-import { HttpClient } from '@angular/common/http';
-import { Product } from './shared/models/product';
-import { Pagination } from './shared/models/pagination';
+import { ShopComponent } from "./features/shop/shop.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
-  url = environment.apiUrl + 'products';
-  http = inject(HttpClient);
-  products: Product[] = [];
-  title = 'client';
-  ngOnInit(): void {
-    this.http.get<Pagination<Product>>(this.url).subscribe((response) => {
-      this.products = response.data;
-      console.log(response);
-    });
-  }
+export class AppComponent  {
+  
 }
