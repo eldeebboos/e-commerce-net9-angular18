2f516b0acb2f53bd4874b45307d0094f444e00b5
eldeebboos <eldeeb_boos@yahoo.com>
2025-05-20 20:00:12 +0300

    add interceptors, error handling and loading spinner

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { MatBadge } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
+import { MatProgressBar } from '@angular/material/progress-bar';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { BusyService } from '../../core/services/busy.service';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, MatIcon, MatBadge, MatButtonModule],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    MatProgressBar,
+    MatIcon,
+    MatBadge,
+    MatButtonModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  busyService = inject(BusyService);
+}
